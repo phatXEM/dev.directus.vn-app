cf8ddb7e73a34b1e8e25bccf09b92701eac853ff
phatXEM <phat@xem.edu.vn>
2025-04-17 14:54:32 +0700

    env

--- env.d.ts
+++ env.d.ts
@@ -2,4 +2,7 @@ declare module '@env' {
   export const API_URL: string;
   export const APPLE_CLIENT_ID: string;
   export const APPLE_REDIRECT_URI: string;
+  export const FACEBOOK_APP_ID: string;
+  export const FACEBOOK_CLIENT_TOKEN: string;
+  export const FACEBOOK_REDIRECT_URI: string;
 }
